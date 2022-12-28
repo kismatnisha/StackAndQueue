65d278bb27d0cdc13be66d0cca371727ec3711a1
qismat <qismatnisha6243@gmail.com>
2022-12-28 08:56:34 +0530

    [qismat] Add : UC1-CreateStack

---
+++ StackAndQueuePrograms/StackAndQueuePrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StackAndQueuePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(56);
+            stack.push(30);
+            stack.push(70);
+            stack.display();
+        }
+    }
+}
+        
