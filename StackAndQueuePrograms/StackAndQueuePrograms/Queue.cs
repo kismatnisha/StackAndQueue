1063604570100aefb244a3698bbbc4e5e8e268e6
qismat <qismatnisha6243@gmail.com>
2022-12-28 09:18:58 +0530

    [qismat] Add : UC3-CreateQueue

---
+++ StackAndQueuePrograms/StackAndQueuePrograms/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueuePrograms
+{
+    internal class Queue
+    {
+        Node front;
+        Node rear;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.rear == null)
+            {
+                this.front = this.rear = newNode;
+            }
+            else
+            {
+                this.rear.next = newNode;
+                this.rear = newNode;
+            }
+            Console.WriteLine($"{data} Inserted into Queue");
+        }
+        public void display()
+        {
+            int i = 1;
+            int size = 0;
+            Node temp = front;
+            if (temp == null)
+            {
+                Console.WriteLine(" Queue is Empty");
+            }
+            while (temp != null)
+            {
+                Console.WriteLine($" Element {i} in the Queue is : " + temp.data);
+                temp = temp.next;
+                size = i;
+                i++;
+            }
+            Console.WriteLine(" Total Element of Size is :" + size);
+        }
+    }
+}
+
+
